24734a551d8cb1364386c9785175d249e8e0233e
Dong <dongbae129@naver.com>
2020-04-30 23:18:25 +0900

    signup and login before passport

--- front/src/sagas/index.js
+++ front/src/sagas/index.js
@@ -1,6 +1,9 @@
-import { all, call } from "redux-saga/effects";
+import { all, fork } from "redux-saga/effects";
+import axios from "axios";
 import user from "./user";
 
+axios.defaults.baseURL = "http://localhost:5000/api";
+
 export default function* rootsaga() {
-  yield all([call(user)]);
+  yield all([fork(user)]);
 }
